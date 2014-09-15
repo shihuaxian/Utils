a3d04db9c8069dcbb43739d82dc13fe7ff476a73
shihx <shihuaxian001@126.com>
2014-09-15 15:30:19 +0800

    make up last commit

--- src/com/shihx/index/CircleActivity.java
+++ src/com/shihx/index/CircleActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class CircleActivity extends Activity {
-	private DIYRoundProgressView progress1,progress2,progress3;
+	private DIYRoundProgressView progress1,progress2,progress3,progress4;
 	private int progress;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +19,7 @@ public class CircleActivity extends Activity {
 		progress1 = (DIYRoundProgressView)findViewById(R.id.circle_one);
 		progress2 = (DIYRoundProgressView)findViewById(R.id.circle_two);
 		progress3 = (DIYRoundProgressView)findViewById(R.id.circle_three);
+		progress4 = (DIYRoundProgressView)findViewById(R.id.circle_four);
 	}
 	
 	public void startProgress(View v){
@@ -33,6 +34,7 @@ public class CircleActivity extends Activity {
 					progress1.setProgress(progress);
 					progress2.setProgress(progress);
 					progress3.setProgress(progress);
+					progress4.setProgress(progress);
 					try {
 						Thread.sleep(100);
 					} catch (InterruptedException e) {
